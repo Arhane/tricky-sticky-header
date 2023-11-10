d40f7a9d6ee84cd73c57c1b7dcdc0007a69852dd
Andrey Lozhkin <lozhkin.andr@gmail.com>
2023-11-10 16:54:54 +0100

    feat: Add the hiding header

--- apps/web/app/components/Header.tsx
+++ apps/web/app/components/Header.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import { useScrollDirection } from '../hooks/useScrollDirection';
+import {  useMemo } from 'react';
 
 export function Header() {
+  const direction = useScrollDirection();
+  const classnames = useMemo(() => {
+    return {
+      transform: direction === 'down' ? '-top-16' : 'top-0',
+    };
+  }, [ direction ]);
+
   return (
     <div
-      className={`sticky top-0 h-16 bg-black transition-all duration-500`}
+      className={`h-16 bg-black transition-all duration-500 ${classnames}`}
     >
-      <div className="p-5 font-bold text-white">Disappearing Header</div>
+      <div className={`p-5 font-bold text-white relative`}>Disappearing Header</div>
     </div>
   );
 }
--- apps/web/app/hooks/useScrollDirection.tsx
+++ apps/web/app/hooks/useScrollDirection.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export function useScrollDirection() {
-
-  const [scrollDirection, setScrollDirection] = useState<'up' | 'down' | null>(
+  const [direction, setDirection] = useState<'up' | 'down' | null>(
     null
   );
 
-    //TODO: implement
+  useEffect(() => {
+    let previousPosition = window.scrollY
 
-  return scrollDirection;
+    const handleScroll = () => {
+      const currentPosition = document.documentElement.scrollTop;
+      const currentDirection = currentPosition > previousPosition ? 'down' : 'up'
+
+      if (currentDirection !== direction) {
+        setDirection(currentDirection)
+      }
+      previousPosition = currentPosition
+    }
+
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [direction])
+
+
+  return direction;
 }
